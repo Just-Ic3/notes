d493cddccc225f26072343dad1c5a2a8e8e97889
Alex Ramirez <alex@iconos.mx>
2019-05-16 18:09:33 -0700

    Initial repositories for Note and User entities.

--- src/main/java/com/disqo/notes/repositories/NoteRepository.java
+++ src/main/java/com/disqo/notes/repositories/NoteRepository.java
@@ -1,4 +1,13 @@
 package com.disqo.notes.repositories;
 
-public interface NoteRepository {
+import com.disqo.notes.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note,String> {
+    List<Note> findAllByUser_Id(String id);
 }
--- src/main/java/com/disqo/notes/repositories/UserRepository.java
+++ src/main/java/com/disqo/notes/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.disqo.notes.repositories;
 
-public interface UserRepository {
+import com.disqo.notes.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+    User findByEmailAndPassword(String email, String password);
 }
