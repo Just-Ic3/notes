0abfba3c358140e0bdd5b877334ee7e8e4c19fb1
Alex Ramirez <alex@iconos.mx>
2019-05-16 20:16:14 -0700

    Added Swagger for API documentation.

---
+++ README.md
@@ -0,0 +1,13 @@
+Welcome to the Notes App!
+Documentation available under: http://localhost:8080/swagger-ui.html
+
+===USER===
+
+Register:
+curl -X POST "http://localhost:8080/users/public/register" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \"email\": \"alex@iconos.mx\",  \"password\": \"alex1234\"}"
+
+Login:
+
+Logout:
+
+===NOTES===
---
+++ src/main/java/com/disqo/notes/configurations/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.disqo.notes.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.host("http://192.168.0.152:8082")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("DISQO - Notes Microservice")
+                .description("REST API for users to create, edit and view their notes.")
+                .version("0.0.1")
+                .build();
+    }
+    //@Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+}
