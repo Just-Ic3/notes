dfdb59eb2d788b9874c1a092af59226314f9a12b
Alex Ramirez <alex@iconos.mx>
2019-05-26 21:06:24 -0700

    Integration tests for NoteController.

--- src/main/java/com/disqo/notes/requests/NoteCreationRequest.java
+++ src/main/java/com/disqo/notes/requests/NoteCreationRequest.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -13,8 +14,10 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NoteCreationRequest {
+    @NotNull(message = "Title must not be null.")
     @Size(min = 1, max = 50, message = "Title must not be empty nor exceed 50 characters.")
     private String title;
+    @NotNull(message = "Note must not be null.")
     @Size(max = 1000, message = "Note must not exceed 1000 characters.")
     private String note;
 }
